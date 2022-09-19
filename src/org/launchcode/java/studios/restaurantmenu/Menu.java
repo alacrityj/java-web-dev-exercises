912f647bc271d06b5cc13b57f10b01abe528667e
alacrityj <alacritydanger@gmail.com>
2022-09-19 02:28:44 -0500

    added getters and setters to Menu and MenuItem

--- src/org/launchcode/java/studios/restaurantmenu/Menu.java
+++ src/org/launchcode/java/studios/restaurantmenu/Menu.java
@@ -1,4 +1,29 @@
 package org.launchcode.java.studios.restaurantmenu;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Menu {
+    private Date lastUpdated;
+    private ArrayList<MenuItem> items;
+    public Menu(Date lastUpdated, ArrayList<MenuItem> items) {
+        this.lastUpdated = lastUpdated;
+        this.items = items;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public ArrayList<MenuItem> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<MenuItem> items) {
+        this.items = items;
+    }
 }
--- src/org/launchcode/java/studios/restaurantmenu/MenuItem.java
+++ src/org/launchcode/java/studios/restaurantmenu/MenuItem.java
@@ -3,13 +3,55 @@ package org.launchcode.java.studios.restaurantmenu;
 public class MenuItem {
     private double price;
     private String description;
+    private String name;
     private String category;
     private boolean isNew;
 
-    public MenuItem(double price, String description, String category, boolean isNew) {
+    public MenuItem(double price, String description, String name, String category, boolean isNew) {
         this.price = price;
         this.description = description;
+        this.name = name;
         this.category = category;
         this.isNew = isNew;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public void setNew(boolean aNew) {
+        isNew = aNew;
     }
 }
